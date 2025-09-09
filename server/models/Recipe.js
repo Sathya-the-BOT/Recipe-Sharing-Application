3d67c3ed6bcb36dba64693736b616abd8c466d00
Sathya Swaroop M S <sathyaswaroop02@gmail.com>
2025-09-09 22:23:31 +0530

    Initial Commits

---
+++ server/models/Recipe.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: 'This field is required.'
+  },
+
+  description: {
+    type: String,
+    required: 'This field is required.'
+  },
+
+  email: {
+    type: String,
+    required: 'This field is required.'
+  },
+
+  ingredients: {
+    type: Array,
+    required: 'This field is required.'
+  },
+
+  category: {
+    type: String,
+    enum:['Thai', 'American', 'Mexican', 'Italian','Indian','Japnese','Korean'],
+    required: 'This field is required.'
+  },
+
+  image: {
+    type: Array,
+    required: 'This field is required.'
+  },
+});
+
+recipeSchema.index({ name: 'text', description: 'text' });
+
+
+module.exports = mongoose.model('Recipe', recipeSchema);
